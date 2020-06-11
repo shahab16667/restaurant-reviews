079470252253173885d385e858a86513ce86a0c1
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-11 11:27:50 +1000

    fixed the bug with chef_name added Faker::Name.unique.name to fix that

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts "database is clean now"
 200.times do
     restaurant = Restaurant.create(
         name: Faker::Restaurant.name,
-        chef_name: Faker::Restaurant.name,
+        chef_name: Faker::Name.unique.name,
         address: Faker::Address.street_address,
         rating: rand(1..5)
     )
