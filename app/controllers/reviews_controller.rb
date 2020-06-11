c18e8f97371a31f9ba2ebfee0ecc22b6de73f070
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-11 12:46:07 +1000

    created a private method to find restaurant id passed the review params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+    before_action :find_restaurant
+    def new 
+        # we need @restaurant in our `simple_form_for`
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @review =  Review.new
+    end
+
+
+    def create
+      @review = Review.new(review_params)
+      @review.restaurant = @restaurant
+      @review.save 
+      redirect_to restaurant_path(@restaurant)
+    end
+
+    private 
+
+    def find_restaurant
+        @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+
+    def review_params 
+        params.require(:review).permit(:content)
+    end
 end
